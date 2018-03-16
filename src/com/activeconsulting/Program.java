0a039e340dcc6e162d1317cc6f47210f57adef9a
mmonsoo <misharl.monsoor@gmail.com>
2018-03-16 16:09:42 +0100

    Ma petite contribution :)

--- src/com/activeconsulting/Program.java
+++ src/com/activeconsulting/Program.java
@@ -4,6 +4,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Quelle est la couleur de ton string ?
+		String [] color= {"Rouge","Bleu","Jaune","Vert","etc..."};
 		
 	}
 
